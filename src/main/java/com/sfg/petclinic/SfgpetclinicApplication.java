05c98c9a592f497135709f811e21425d2ce1d62c
suprajkangod <suprajkangod@gmail.com>
2020-06-02 21:49:58 +0530

    First commit

---
+++ src/main/java/com/sfg/petclinic/SfgpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgpetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfg/petclinic/SfgpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgpetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
